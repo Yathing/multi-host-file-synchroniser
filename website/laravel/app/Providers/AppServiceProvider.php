41f4181b1e2e518d51c7ba0b4eb1707c27c020ba
Tom Robinson <46812835+tomrobinson1996@users.noreply.github.com>
2019-02-08 15:22:14 +0000

    Fixing 'key too long' error
    Was unable to open webpage properly without adding:
    
    use Illuminate\Support\Facades\Schema;
    
    public function boot()
    {
        Schema::defaultStringLength(191);
    }

--- website/laravel/app/Providers/AppServiceProvider.php
+++ website/laravel/app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        Schema::defaultStringLength(191);
+
         Blade::if('isAdmin', function(){
 
             if (Auth::check()) 
