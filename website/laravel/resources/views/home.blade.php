e980053bca44464bad97b2d3db52a7ad1f48e651
Trisha-Eun <46794573+Trisha-Eun@users.noreply.github.com>
2019-03-04 04:28:23 +0000

    Update home.blade.php

--- website/laravel/resources/views/home.blade.php
+++ website/laravel/resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
                 <div class="row bg-title">
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">主控台</h4> 
+                        <h4 class="page-title">Main Page</h4> 
                     </div>
                 </div>
 
