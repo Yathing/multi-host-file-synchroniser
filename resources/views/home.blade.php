49fe042a761d75801553a7e3cdfbeade786657f4
tomrobinson1996 <thomas.robinson@kcl.ac.uk>
2019-03-04 16:08:57 +0000

    delete useless

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.nav')
-@section('content')
-                <div class="row bg-title">
-                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">主控台</h4> 
-                    </div>
-                </div>
-
-@endsection
