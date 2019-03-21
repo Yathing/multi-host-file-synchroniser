8e59499a3e2628a83c26cb4045c1550f699a0659
Marcell Sarosi <marcell.sarosi@kcl.ac.uk>
2019-03-21 10:30:13 +0000

    Add all starting Android code
    Added all Android code for selecting and uploading files, as well as login UI

---
+++ android code/FileSync/app/src/main/java/com/lucky/filesync/MyLogin.java
@@ -0,0 +1,19 @@
+package com.lucky.filesync;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MyLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_login);
+    }
+
+    public void doLogin(View view) {
+
+    }
+}
